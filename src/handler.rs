becef414fc329af8f563c6c26cd4416d6bfabc0a
tanacchi <q111026d@mail.kyutech.jp>
2022-06-11 22:39:49 +0900

    Redirect from / (root) to /ui/top

--- src/handler.rs
+++ src/handler.rs
@@ -1,13 +1,19 @@
-use actix_web::{web, get, HttpResponse, Responder, Result};
+use actix_web::{
+    web, get, HttpResponse, Responder, Result,
+    http::header,
+};
 use crate::db;
 
 
 #[get("/")]
 pub async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    let destination = "/ui/top";
+    HttpResponse::TemporaryRedirect()
+        .append_header((header::LOCATION, destination))
+        .finish()
 }
 
-#[get("hello")]
+#[get("/top")]
 pub async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
