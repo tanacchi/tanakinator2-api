d37ca01a60c0e9ed189b9af629e9fc392b50c9d6
tanacchi <q111026d@mail.kyutech.jp>
2022-06-18 23:05:53 +0900

    Add settings of logger

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,18 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger};
 use tanakinator2_api::{handler, routes};
-
+use env_logger;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(
+        env_logger::Env::new().default_filter_or("info")
+    );
+
     HttpServer::new(|| {
         App::new()
             .service(handler::common::root)
             .configure(routes::app_config)
+            .wrap(Logger::default())
     })
     .bind(("127.0.0.1", 8080))?
     .run()
