0fda53b58e32b4db31863aa69fc13f84738fc0ab
tanacchi <q111026d@mail.kyutech.jp>
2022-06-18 17:35:49 +0900

    Change redirect destination of root page

--- src/handler/common.rs
+++ src/handler/common.rs
@@ -6,7 +6,7 @@ use actix_web::{
 
 #[get("/")]
 pub async fn root() -> impl Responder {
-    let destination = "/ui/top";
+    let destination = "/ui/list";
     HttpResponse::TemporaryRedirect()
         .append_header((header::LOCATION, destination))
         .finish()
