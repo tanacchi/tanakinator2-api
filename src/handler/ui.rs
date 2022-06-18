a9c20fad0532e50547429f380b86879a9094200e
tanacchi <q111026d@mail.kyutech.jp>
2022-06-18 17:33:49 +0900

    Remove unnecessary code

--- src/handler/ui.rs
+++ src/handler/ui.rs
@@ -32,7 +32,7 @@ pub async fn manual_hello() -> impl Responder {
 
 #[get("/new")]
 pub async fn render_new_question_form() -> impl Responder {
-    let mut context = Context::new();
+    let context = Context::new();
     let html_body = match TEMPLATES.render("add.html", &context) {
         Ok(s) => s,
         Err(e) => {
@@ -74,7 +74,7 @@ pub async fn list_questions() -> impl Responder {
     .await
     .unwrap();
     println!("{:?}", &questions);
-    let questions: Questions = json::parse(&questions).unwrap().into();
+    let questions: Questions = serde_json::from_str(&questions).unwrap();
     let questions: Vec<Question> = questions.into();
 
     let mut context = Context::new();
@@ -91,32 +91,4 @@ pub async fn list_questions() -> impl Responder {
             ContentType::html()
         )
         .body(html_body)
-}
-
-
-#[cfg(test)]
-mod tests {
-    use crate::models::{Question, Questions};
-
-    #[test]
-    fn should_parse_json_to_questions() {
-        use json;
-
-        let response_body = "[{\"id\":1,\"body\":\"Question 1\"},{\"id\":2,\"body\":\"Question 2\"},{\"id\":3,\"body\":\"Question 3\"}]";
-        let parsed = json::parse(response_body).unwrap();
-
-        assert_eq!(1, parsed[0]["id"]);
-        assert_eq!("Question 1", parsed[0]["body"]);
-
-        let expected  = Question{ id: 1, body: Some(String::from("Question 1"))};
-        assert_eq!(expected, Question::from(&parsed[0]));
-
-        let _expected = vec![
-            Question{ id: 1, body: Some(String::from("Question 1"))},
-            Question{ id: 2, body: Some(String::from("Question 2"))},
-            Question{ id: 3, body: Some(String::from("Question 3"))},
-        ];
-        let expected = Questions::new(_expected);
-        assert_eq!(expected, Questions::from(parsed))
-    }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,51 +1,19 @@
-use std::convert::From;
-use json::JsonValue;
 use crate::schema::questions;
 use serde::{Serialize, Deserialize};
 
 
-#[derive(Queryable, Debug, Serialize, PartialEq)]
+#[derive(Queryable, Debug, Serialize, Deserialize, PartialEq)]
 pub struct Question {
     pub id: i64,
     pub body: Option<String>,
 }
 
 
-impl From<&JsonValue> for Question {
-    fn from(json: &JsonValue) -> Self {
-        Question {
-            id: json["id"].as_i64().unwrap(),  // TODO: Make it general.
-            body: json["body"].as_str().map(String::from)
-        }
-    }
-}
-
-
-#[derive(Debug, Serialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Questions ( Vec<Question> );
 
 
-impl Questions {
-    pub fn new(questions: Vec<Question>) -> Self {
-        Questions(questions)
-    }
-}
-
-
-impl From<JsonValue> for Questions {
-    fn from(_: JsonValue) -> Self {  // TODO: Implement it
-        let expected = vec![
-            Question{ id: 1, body: Some(String::from("Question 1"))},
-            Question{ id: 2, body: Some(String::from("Question 2"))},
-            Question{ id: 3, body: Some(String::from("Question 3"))},
-        ];
-        Questions(expected)
-    }
-}
-
-
-// Questions は必要ないかもしれない
-impl Into<Vec<Question>> for Questions {  // for XXX の XXX が独自じゃないといけないだけ
+impl Into<Vec<Question>> for Questions {
     fn into(self) -> Vec<Question> {
         self.0
     }
